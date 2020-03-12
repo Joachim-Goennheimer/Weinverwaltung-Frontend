0d493b77d52043f834c3995a4098efa506300bcf
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-03-12 17:49:44 +0100

    first draft wineView

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -1,6 +1,45 @@
+const { remote } = require('electron')
+const { BrowserWindow } = remote
+
+function loadVerkaufslisteView() {
+    $('#currentViewContent').load('./views/verkaufslisteView.html');
+
+}
+
+function loadWineView() {
+    $('#currentViewContent').load('./views/wineView.html');
+
+}
+
 function loadCustomerView() {
 
     $('#currentViewContent').load('./views/customerView.html');
 
+}
+
+function loadWinzerView() {
+
+    $('#currentViewContent').load('./views/winzerView.html');
+
+}
+
+function logoutUser() {
+
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    win.loadFile('./src/loginWindow.html')
+    win.webContents.openDevTools()
+
+    remote.getCurrentWindow().close();
+
+
+}
+
+function doBackup() {
 
 }
